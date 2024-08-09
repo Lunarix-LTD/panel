52be40e66e2426a735fd9398c916160ad6e1e38a
Nezixyan <146237851+Nezixyan@users.noreply.github.com>
2024-08-09 11:43:55 +0300

    Update index.js

--- exec/bin/index.js
+++ exec/bin/index.js
@@ -11,7 +11,7 @@ async function loadChalk() {
 
 program
     .version("0.1.0-beta6")
-    .description("Command Line Interface for the Skyport Panel");
+    .description("Command Line Interface for the Lunarix Panel");
 
 program
     .command('seed')
